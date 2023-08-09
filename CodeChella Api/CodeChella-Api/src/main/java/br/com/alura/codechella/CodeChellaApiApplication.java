43389fe82373797fdd02a9e1132dab4fa8c67b7f
lucasmoraist <luksmnt1101@gmail.com>
2023-08-09 10:58:46 -0300

    Template da API do codechella

---
+++ CodeChella Api/CodeChella-Api/src/main/java/br/com/alura/codechella/CodeChellaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.codechella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeChellaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeChellaApiApplication.class, args);
+	}
+
+}

46b7c2c6f2ba76808b93314c95bee7af5538b3db
lucasmoraist <luksmnt1101@gmail.com>
2023-08-09 14:16:03 -0300

    Criando CRUD para consumir a API

---
+++ CodeChella Api/CodeChella-Api/src/main/java/br/com/alura/codechella/controller/PessoaController.java
@@ -0,0 +1,85 @@
+package br.com.alura.codechella.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+
+import br.com.alura.codechella.model.Pessoa;
+import br.com.alura.codechella.model.Request;
+import br.com.alura.codechella.model.Response;
+import br.com.alura.codechella.repositories.PessoaRepository;
+
+@RestController
+@RequestMapping("/pessoa")
+public class PessoaController {
+    
+    @Autowired
+    PessoaRepository repository;
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping
+    public List<Response> getAll(){
+
+        List<Response> pessoas = repository.findAll().stream().map(Response::new).toList();
+        
+        return pessoas;
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Pessoa> show(@PathVariable Long id){
+        Optional<Pessoa> pessoaOptional = repository.findById(id);
+
+        if (pessoaOptional.isPresent()) {
+            Pessoa pessoa = pessoaOptional.get();
+            return ResponseEntity.ok(pessoa);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping
+    public void reserva(@RequestBody Request data){
+        Pessoa p = new Pessoa(data); 
+
+        repository.save(p);
+        return;
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Pessoa> update(@PathVariable Long id, @RequestBody @Valid Pessoa pessoa){
+        repository.findById(id);
+
+        pessoa.setId(id);
+        repository.save(pessoa);
+
+        return ResponseEntity.ok(pessoa);
+
+    }   
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Pessoa> destroy(@PathVariable Long id){
+        Optional<Pessoa> pessoaOptional = repository.findById(id);
+
+        if (pessoaOptional.isPresent()) {
+            Pessoa pessoa = pessoaOptional.get();
+            repository.delete(pessoa);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
+    }
+}

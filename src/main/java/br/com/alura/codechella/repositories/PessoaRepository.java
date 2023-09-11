02d6cb4f04c2a5943dd672b8f188c91a66829f4b
lucasmoraist <luksmnt1101@gmail.com>
2023-09-11 14:16:50 -0300

    Adicionando novas dependencias

--- src/main/java/br/com/alura/codechella/repositories/PessoaRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.alura.codechella.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.alura.codechella.model.Pessoa;
-
-public interface PessoaRepository extends JpaRepository<Pessoa, Long> {}

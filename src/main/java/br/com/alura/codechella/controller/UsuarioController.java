a5334052ffe9d433b029d398eaf369e750e42b29
lucasmoraist <luksmnt1101@gmail.com>
2023-10-04 11:27:33 -0300

    Removendo importação não utilizada

--- src/main/java/br/com/alura/codechella/controller/UsuarioController.java
+++ src/main/java/br/com/alura/codechella/controller/UsuarioController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.codechella.model.Credencial;

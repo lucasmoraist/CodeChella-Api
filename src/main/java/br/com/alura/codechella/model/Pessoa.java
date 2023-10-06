7f27c90d1778bbbf263f8d78ab88aecf9feca1fc
lucasmoraist <luksmnt1101@gmail.com>
2023-10-06 10:15:40 -0300

    Removendo importações inutilizaveis

--- src/main/java/br/com/alura/codechella/model/Pessoa.java
+++ src/main/java/br/com/alura/codechella/model/Pessoa.java
@@ -5,18 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Builder
-@Table(name = "T_PESSOA")
 public class Pessoa { 
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,12 +22,11 @@ public class Pessoa {
     
     private String cpf;
 
-    private String setor;
 
     private LocalDate dtNasc;
 
     @Override
     public String toString(){
-        return "Pessoa [Nome: " +nome+ "\nEmail: " +email+ "\nCpf: " +cpf+ "\nSetor: " +setor+ "\nData de nascimento: " +dtNasc+ "]";
+        return "Pessoa [Nome: " +nome+ "\nEmail: " +email+ "\nCpf: " +cpf+ "\nData de nascimento: " +dtNasc+ "]";
     }
 }
